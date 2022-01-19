7f651e75c973f85e37fb4e4570b145562cebe807
Daisuke <daisuke@h2a.sakura.ne.jp>
2022-01-20 08:10:22 +0900

    Update bot_license_search.php

--- bot_license_search.php
+++ bot_license_search.php
@@ -42,7 +42,12 @@ function mic_get($callsign){
 	$res = $res . "```json\n";
 	$res = $res . "呼出符号: " . $arr["musen"][0]["detailInfo"]["identificationSignals"] ."\n";
 	$res = $res . "　　名称: " . nmz($arr["musen"][0]["detailInfo"]["name"], "as", "utf8") . "\n";
-	$res = $res . "免許期間: " . $arr["musen"][0]["detailInfo"]["licenseDate"] . " => " . preg_replace('/[^a-zA-Z0-9_-]/', '', $arr["musen"][0]["detailInfo"]["validTerms"]) ."\n";
+	$arr["musen"][0]["detailInfo"]["validTerms"] = preg_replace('/[^a-zA-Z0-9_-]/', '', $arr["musen"][0]["detailInfo"]["validTerms"]);	//「まで」を除去
+	$d1 = new DateTime("now");
+	$d1 -> setTime(0,0,0);
+	$d2 = new DateTime($arr["musen"][0]["detailInfo"]["validTerms"]);
+	$interval = $d2->diff($d1);
+	$res = $res . "免許期間: " . $arr["musen"][0]["detailInfo"]["licenseDate"] . " => " . preg_replace('/[^a-zA-Z0-9_-]/', '', $arr["musen"][0]["detailInfo"]["validTerms"]) . " (" . $interval->format("残り%y年%m月%d日") . ")" . "\n";
 	$res = $res . "常置場所: " . nmz($arr["musen"][0]["detailInfo"]["radioEuipmentLocation"]) . "\n";
 	$res = $res . "移動範囲: " . $arr["musen"][0]["detailInfo"]["movementArea"] ."\n";
 	$res = $res . "　周波数: ";
@@ -50,8 +55,10 @@ function mic_get($callsign){
 	foreach($radioSpec_arr as $index => $tmp1){
 		$tmp1 = str_replace(" ", "", $tmp1);
 		$tmp2 = explode("\\t", $tmp1);
-		$res = $res . nmz($tmp2[1] . "(" . $tmp2[2] . ")");
-		if ($index != array_key_last($radioSpec_arr)){ $res = $res . ", "; }
+		if($tmp2[1] != ""){
+			$res = $res . nmz($tmp2[1] . "(" . $tmp2[2] . ")");
+			if ($index != array_key_last($radioSpec_arr)){ $res = $res . ", "; }
+		}
 	}
 	$res = $res . "```\n";
 	return $res;
@@ -62,6 +69,6 @@ function nmz($text){
 	$tmp = mb_convert_kana($text, "as", "utf8");	//英数スペースの半角変換
 	$tmp = trim($tmp);	//前後スペース除去
 	$tmp = preg_replace("/[\s]+/", " ", $tmp);	//連続スペースの除去
-	$tmp = str_replace("\\n", " ", $tmp);
+	$tmp = str_replace("\\n", " ", $tmp);	//改行らしき文字の除去
 	return $tmp;
 }
